163df0a578aa37025d1658512aef9e5b7055ffef
pranaboy72 <86711384+pranaboy72@users.noreply.github.com>
2022-09-07 12:51:08 +0900

    Update link_robcon.ino

--- link_robcon/link_robcon.ino
+++ link_robcon/link_robcon.ino
@@ -21,15 +21,17 @@ const uint8_t ID_1 = 1; //CONTROLLER JOINT1
 const uint8_t ID_2 = 2; //ROBOT JOINT1
 const uint8_t ID_3 = 3; //ROBOT JOINT2
 const uint8_t ID_4 = 4; //CONTROLLER JOINT2
-const uint8_t ID_5 = 5; //CONTROLLER ROTATION
-const uint8_t ID_6 = 6; //ROBOT ROTATION
+const uint8_t ID_5 = 5; //ROBOT ROTATION1
+const uint8_t ID_6 = 6; //CONTROLLER ROTATION
+const uint8_t ID_7 = 7; //ROBOT ROTATION2
 const int32_t BAUDRATE = 57600;
 
-float a=68.73, b=46.82;
-double A,B;
-double theta2, theta3, theta5;
+float a=68.73, b=46.82, c=14.25;
+double A,B,E,F;
+double theta2, theta3, theta5, alpha, rot_ang;
 double vel_1, vel_0, pos, vel1_1, vel0_1;
 int i;
+int DEBUG = 1;
 
 using namespace ControlTableItem;
 
@@ -44,6 +46,7 @@ void setup() {
   dxl.ping(ID_4);
   dxl.ping(ID_5);
   dxl.ping(ID_6);
+  dxl.ping(ID_7);
   
   dxl.setOperatingMode(ID_1, OP_CURRENT_BASED_POSITION);
   dxl.setOperatingMode(ID_2, OP_CURRENT_BASED_POSITION);
@@ -51,28 +54,51 @@ void setup() {
   dxl.setOperatingMode(ID_4, OP_CURRENT_BASED_POSITION);
   dxl.setOperatingMode(ID_5, OP_CURRENT_BASED_POSITION);
   dxl.setOperatingMode(ID_6, OP_CURRENT_BASED_POSITION);
-
+  dxl.setOperatingMode(ID_7, OP_CURRENT_BASED_POSITION);
+  
+  dxl.ledOn(ID_1);
+  dxl.ledOn(ID_2);
+  dxl.ledOn(ID_3);
+  dxl.ledOn(ID_4);
+  dxl.ledOn(ID_5);
+  dxl.ledOn(ID_6);
+  dxl.ledOn(ID_7);
+  delay(1000);
+  
+  dxl.ledOff(ID_1);
+  dxl.ledOff(ID_2);
+  dxl.ledOff(ID_3);
+  dxl.ledOff(ID_4);
+  dxl.ledOff(ID_5);
+  dxl.ledOff(ID_6);
+  dxl.ledOff(ID_7);
+  delay(1000);
+  
+  soft_serial.begin(115200);
+  soft_serial.println("Set up complete");
+  
+  i=0;
+  
   dxl.torqueOn(ID_2);
   dxl.torqueOn(ID_3);
-  
-  get_angle();  
-
-  while(theta2 == dxl.getPresentPosition(ID_2, UNIT_DEGREE) && theta3 == dxl.getPresentPosition(ID_3)){
-    dxl.setGoalPosition(ID_2, theta2, UNIT_DEGREE);
-    dxl.setGoalPosition(ID_3, theta3, UNIT_DEGREE);
-  }
+  dxl.torqueOn(ID_5);
+  dxl.torqueOn(ID_7);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  get_angle();
+  
   vel_1 = dxl.getPresentVelocity(ID_1);
   vel_0 = dxl.getPresentVelocity(ID_4);
   
-  get_angle();
+  /* 이 부분 로봇의 모터 위치에 맞게 수정 필요 */
   dxl.setGoalPosition(ID_2, theta2, UNIT_DEGREE);
   dxl.setGoalPosition(ID_3, theta3, UNIT_DEGREE);
+  dxl.setGoalPosition(ID_5, rot_ang, UNIT_DEGREE);
+  dxl.setGoalPosition(ID_7, rot_ang, UNIT_DEGREE);
 
   if ((dxl.getPresentVelocity(ID_2) == 0 || dxl.getPresentVelocity(ID_3) == 0) && (vel_1 * dxl.getPresentVelocity(ID_1) > 0 || vel_0 * dxl.getPresentVelocity(ID_4))){
+    // 로봇이 못 움직이는 경우
     while(1){
       dxl.torqueOn(ID_1);
       dxl.torqueOn(ID_4);
@@ -94,34 +120,38 @@ void loop() {
     }
   }
 
-  if (dxl.getPresentVelocity(ID_5)>0) dxl.setGoalPosition(ID_6, dxl.getPresentPosition(ID_5));
+  // if (dxl.getPresentVelocity(ID_5)>0) dxl.setGoalPosition(ID_6, dxl.getPresentPosition(ID_5));
 
   i++;
-  if (i==99){
+  if (DEBUG && i==99){
     i=0;
     soft_serial.print("Theta2: ");
     soft_serial.println(theta2);
 
+    soft_serial.print("Theta5: ");
+    soft_serial.println(theta5);
+
+
     soft_serial.print("Theta3: ");
     soft_serial.println(theta3);
+
+    soft_serial.print("Rotation Angle: ");
+    soft_serial.println(rot_ang);
+    soft_serial.println("");
   }
-}
-
-uint32_t multiply(uint32_t multiplicand, float multiplier){
-  const uint32_t TwoToThe24th = 0x01000000;
-  uint64_t intMultiplier = multiplier * TwoToThe24th;
-
-  intMultiplier *= multiplicand;
-
-  uint64_t result = intMultiplier / TwoToThe24th;
-  return result;
 }
 
 void get_angle(){ 
   theta2 = dxl.getPresentPosition(ID_1, UNIT_DEGREE);
+  theta2 = theta2 - 270.0;
   theta5 = dxl.getPresentPosition(ID_4, UNIT_DEGREE);
-  A = b/a *(sin(theta2) + sin(theta5));
-  B = b/a *(cos(theta2) + cos(theta5));
-  theta3 = atan(A/B) - theta2 * M_PI/180.0;
-  theta3 = 90.0 + theta3 * 180.0/M_PI;
+  theta5 = theta5 + 45.0;
+  A = b/a *(sin(M_PI/180.0*theta2) - sin(M_PI/180.0*theta5));
+  B = b/a *(cos(M_PI/180.0*theta2) - cos(M_PI/180.0*theta5));
+  E = pow(b,2)/pow(a,2)*(cos(theta2*M_PI/180.0 - theta5*M_PI/180.0)-1)-4*pow(c,2)/pow(a,2)-4*c/a*B;
+  F = sqrt(pow(A,2) + pow(B+4*c/a,2));
+  alpha = asin((B+4*c/a)/F)*180.0/M_PI;
+  theta3 = asin(E/F) - theta2 - alpha;
+  theta3 = theta3 + 180.0;
+  rot_ang = dxl.getPresentPosition(ID_5, UNIT_DEGREE);
 }
